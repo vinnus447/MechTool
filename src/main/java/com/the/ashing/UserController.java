efa3bbe0fd65bc241f408a95cf2bbab13eec76b5
vinnus447 <vinodpadala92.cool@gmail.com>
2020-03-08 01:09:00 +0530

    updating files

--- src/main/java/com/the/ashing/UserController.java
+++ src/main/java/com/the/ashing/UserController.java
@@ -1,7 +1,7 @@
 /**
  * 
  */
-package com.the.ashing.controller;
+package com.the.ashing;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
