205c023ea98415d55377097b2bf8425619feb161
vinnus447 <vinodpadala92.cool@gmail.com>
2020-03-08 13:50:31 +0530

    adding application

--- src/main/java/com/the/ashing/Application.java
+++ src/main/java/com/the/ashing/Application.java
@@ -5,13 +5,15 @@ package com.the.ashing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 /**
  * @author vinod
  *
  */
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer{
 
 	/**
 	 * @param args
@@ -19,5 +21,11 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+
 
 }
